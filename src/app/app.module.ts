fc7f8482f4c4241b7635689cd3c82cdd37b4ae97
MSreeN <khyathimnsree369@gmail.com>
2023-05-08 12:49:45 +0530

    added basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent
@@ -15,4 +15,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
